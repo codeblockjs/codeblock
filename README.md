afc0dc142d244568b52cfe0c4f0a64701bb8bf86
Jovica Aleksic <jovica.aleksic@loopmode.de>
2020-02-12 11:26:17 +0100

    chore(storybook): add info and check for .gh-token file

---
+++ README.md
@@ -0,0 +1,9 @@
+### Publishing storybook
+
+To publish the storybook to GitHub pages, you must first create a file `.gh-token` and put a valid GitHub access token in it.
+
+Then you can run the command to build and deploy the storybook:
+
+```sh
+./deploy-storybook.sh
+```
--- deploy-storybook.sh
+++ deploy-storybook.sh
@@ -3,4 +3,10 @@
 # it didn't seem to work directly within package scripts, also not using cross-env
 # it does, however, easily work using a shellscript
 
+if [ ! -f ".gh-token" ]; then
+    echo "File does not exist: .gh-token"
+    exit 1
+fi
+
+yarn build-storybook
 export GH_TOKEN=`cat .gh-token` && yarn storybook-to-ghpages --existing-output-dir=storybook-static
