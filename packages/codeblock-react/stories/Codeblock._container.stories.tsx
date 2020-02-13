fd9b8bff2f2f4634d3eba9cd862fd28c962092dc
Jovica Aleksic <jovica.aleksic@loopmode.de>
2020-02-13 11:14:18 +0100

    style(storybook): use CSS grid in container story

--- packages/codeblock-react/stories/Codeblock._container.stories.tsx
+++ packages/codeblock-react/stories/Codeblock._container.stories.tsx
@@ -4,12 +4,12 @@ import raw from 'raw.macro';
 import { withKnobs, select } from '@storybook/addon-knobs';
 
 import { availableThemes } from '@codeblock/core';
-import Codeblock from '@codeblock/react/static';
+import Codeblock from '@codeblock/react/cdn';
 export default { title: 'Codeblock.react', decorators: [withKnobs] };
 
 export const AsContainer = () => (
   <div>
-    <h3>Container mode</h3>
+    <h2>Container mode</h2>
     <p>
       Here, a single codeblock is used for multiple elements with individual
       languages. Typical scenario is rendered markdown.
@@ -18,17 +18,31 @@ export const AsContainer = () => (
       isContainer
       theme={select('Theme', ['', ...availableThemes], 'okaidia')}
     >
-      <div>
+      <div className="grid-2">
         <div>
-          <pre className="language-jsx">{raw('../src/Codeblock.http.tsx')}</pre>
+          <h3>Own source</h3>
+          <pre className="language-tsx">
+            {raw('./Codeblock._container.stories.tsx')}
+          </pre>
         </div>
 
         <div>
+          <h3>Own package.json</h3>
           <pre className="language-json">{raw('../package.json')}</pre>
         </div>
 
         <div>
-          <pre className="language-json">{raw('../../../package.json')}</pre>
+          <h3>Own tsconfig.json</h3>
+          <div>
+            <pre className="language-json">{raw('../tsconfig.json')}</pre>
+          </div>
+        </div>
+
+        <div>
+          <h3>Another codeblock</h3>
+          <pre className="language-tsx">
+            {raw('../src/Codeblock.static.tsx')}
+          </pre>
         </div>
       </div>
     </Codeblock>
