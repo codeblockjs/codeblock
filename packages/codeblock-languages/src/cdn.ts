99ac9fb57c3cc2dbf0fd5540e2502967fdd239ae
Jovica Aleksic <jovica.aleksic@loopmode.de>
2020-02-14 12:37:59 +0100

    feat: add cdn and http providers

---
+++ packages/codeblock-languages/src/cdn.ts
@@ -0,0 +1,4 @@
+import { CDN_AUTOLOAD_PATH } from '@codeblock/core/lib/http';
+import { createHttpLanguageProvider } from '@codeblock/core/lib/http';
+
+export default createHttpLanguageProvider(CDN_AUTOLOAD_PATH);
---
+++ packages/codeblock-languages/src/http.ts
@@ -0,0 +1,4 @@
+import { HTTP_AUTOLOAD_PATH } from '@codeblock/core/lib/http';
+import { createHttpLanguageProvider } from '@codeblock/core/lib/http';
+
+export default createHttpLanguageProvider(HTTP_AUTOLOAD_PATH);
